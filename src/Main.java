ad0267f0cf412262a5a334dd30182feefb403ae8
MeliLiz <melifernandez@ciencias.unam.mx>
2022-05-14 14:42:01 -0500

    falta caso de eliminacion de hojas negras

---
+++ src/Main.java
@@ -0,0 +1,60 @@
+package src.edd;
+
+import java.util.Iterator;
+
+/**
+ * Clase de prueba de los metodos de arboles
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+        System.out.println("PRUEBAS ÁRBOL ROJINEGRO");
+
+        ArbolRojinegro<Integer> arbol=new ArbolRojinegro<Integer>();
+        System.out.println("Agregamos el 21 y el 15");
+        arbol.insertar(21);
+        System.out.println(arbol);
+        arbol.insertar(15);
+        System.out.println(arbol);
+        System.out.println("Agregamos el 7");
+        arbol.insertar(7);
+        System.out.println(arbol);
+        System.out.println("Agregamos el 80");
+        arbol.insertar(80);
+        
+        System.out.println(arbol);
+        System.out.println("Agregamos el 79");
+        arbol.insertar(79);
+        //arbol.delete(21);
+        System.out.println(arbol);
+        System.out.println("Agregamos el 8");
+        arbol.insertar(8);
+        System.out.println(arbol);
+        System.out.println("Agregamos el 60");
+        arbol.insertar(60);
+        System.out.println(arbol);
+        System.out.println("Eliminamos el 21");
+        arbol.delete(21);
+        System.out.println(arbol);
+        System.out.println("Eliminamos el 8");
+        arbol.delete(8);
+        System.out.println(arbol);
+        System.out.println("Eliminamos el 15");
+        arbol.delete(15);
+        System.out.println(arbol);
+        
+        /*System.out.println("Creamos un arbol a partir de una lista");
+        Lista<Integer> lista=new Lista<Integer>();
+        lista.add(20);
+        lista.add(15);
+        lista.add(10);
+        lista.add(5);
+        lista.add(17);
+        lista.add(2);
+        lista.add(7);
+        lista.add(7);
+        ArbolRojinegro<Integer> a =new ArbolRojinegro<Integer>(lista,false, false);
+        System.out.println(a);*/
+        
+    }
+}
